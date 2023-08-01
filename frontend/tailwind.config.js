74e53464cb0991541400acbfacfcfb0a067c7165
Doruk Gezici <doruk@gezici.me>
2023-08-01 16:04:17 +0200

    Prettier

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': 'var(--primary)',
-        'dark': 'var(--dark)',
-        'light': 'var(--light)',
-        'lighter': 'var(--lighter)'
-      }
-    }
+        primary: "var(--primary)",
+        dark: "var(--dark)",
+        light: "var(--light)",
+        lighter: "var(--lighter)",
+      },
+    },
   },
-  plugins: [require('daisyui')]
+  plugins: [require("daisyui")],
 };
