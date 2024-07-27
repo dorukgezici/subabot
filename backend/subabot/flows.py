f16a1ebdd4803232009033f7daa9699c768ee24d
Doruk Gezici <doruk@gezici.me>
2024-07-28 00:07:03 +0200

    imports fixed

--- backend/subabot/flows.py
+++ backend/subabot/flows.py
@@ -1,3 +1,4 @@
+import asyncio
 from prefect import flow, task
 
 from .rss.crawler import run_crawler as _run_crawler
@@ -6,3 +7,11 @@ from .rss.crawler import run_crawler as _run_crawler
 @flow(log_prints=True)
 async def rss_crawler():
     await _run_crawler()
+
+
+if __name__ == "__main__":
+    asyncio.run(rss_crawler.deploy(
+        name="subabot",
+        work_pool_name="workers",
+        image="dorukgezici/subabot:prefect",
+    ))
